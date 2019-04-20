7fae7a4ef3b050d5eef469cfc571553f05aac38e
sjtype <31806705+sjtype@users.noreply.github.com>
2019-04-19 23:06:51 -0400

    internal documentation

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,10 +1,13 @@
-/*  File name: COMP308-W2019-Assignment01
+/*
+    File name: COMP308-W2019-Assignment01
     Author's name: Semy Jin
     Student ID: 300910535
     Web app name: Express Portfolio
-    Github repository link: 
-    Heroku link: 
-    */
+    Github repository link: https://github.com/sjtype/COMP308-W2019-Assignment01-300910535
+    Heroku link: https://comp308-w2019-a1-300910535.herokuapp.com/
+  */
+
+// confirm that message was sent
 function confirmMessage(){
     var firstName = document.getElementById("txtFirstName").value;
     //var lastName = document.getElementById("txtLastName").value;
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
+/*
+    File name: COMP308-W2019-Assignment01
+    Author's name: Semy Jin
+    Student ID: 300910535
+    Web app name: Express Portfolio
+    Github repository link: https://github.com/sjtype/COMP308-W2019-Assignment01-300910535
+    Heroku link: https://comp308-w2019-a1-300910535.herokuapp.com/
+  */
+
 let express = require('express');
 let router = express.Router();
 
