4fff2703557f7f2c3468247bb46992fce746fa65
sjtype <31806705+sjtype@users.noreply.github.com>
2019-04-19 23:00:38 -0400

    modified contact page

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,11 +1,15 @@
-// IIFE -- Immediately Invoked Function Expression
-(function(){
+/*  File name: COMP308-W2019-Assignment01
+    Author's name: Semy Jin
+    Student ID: 300910535
+    Web app name: Express Portfolio
+    Github repository link: 
+    Heroku link: 
+    */
+function confirmMessage(){
+    var firstName = document.getElementById("txtFirstName").value;
+    //var lastName = document.getElementById("txtLastName").value;
+    //var email = document.getElementById("txtEmail").value;
+    //var message = document.getElementById("txtMessage").value;
 
-    function Start() {
-        console.log('%c App Started...', 
-        "font-size: 20 px; color: blue; font-weight: bold;")
-
-    }
-
-    window.addEventListener("load", Start);
-})();
+    alert(firstName + ", your message has been sent.");
+}
